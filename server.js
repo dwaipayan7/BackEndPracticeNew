ce29223eb0c02d2e38a4d2ff4cefb20afd0f8d00
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-07 10:54:04 +0530

    updated

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/paneer', (req, res)=>{
     res.send("Paneer is a very tasty")
 })
 
+app.get('/idli', (req,res)=>{
+    var customized = {
+        name: 'Paneer Dhosa',
+        size: '10 cm',
+        is_sambar: true,
+        is_chutney: false
+    }
+    res.send(customized)
+})
+
 app.listen(3000, ()=>{
     console.log("Server Running");
     
