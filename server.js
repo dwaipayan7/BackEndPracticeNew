a5409b118ff74ba95c65e8005f6845a8b317a21e
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-09 11:05:56 +0530

    updated

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 
-app.get('/', function(req, res){
-    res.send("Dwaipayan")
-});
+const bodyParser = require('body-parser');
 
-app.get('/paneer', (req, res)=>{
-    res.send("Paneer is a very tasty")
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/idli', (req,res)=>{
-    var customized = {
-        name: 'Paneer Dhosa',
-        size: '10 cm',
-        is_sambar: true,
-        is_chutney: false
-    }
-    res.send(customized)
-})
+
 
 app.listen(3000, ()=>{
     console.log("Server Running");
     
 })
+
+
+
+// app.get('/', function(req, res){
+//     res.send("Dwaipayan")
+// });
+
+// app.get('/paneer', (req, res)=>{
+//     res.send("Paneer is a very tasty")
+// })
+
+// app.get('/idli', (req,res)=>{
+//     var customized = {
+//         name: 'Paneer Dhosa',
+//         size: '10 cm',
+//         is_sambar: true,
+//         is_chutney: false
+//     }
+//     res.send(customized)
+// })
