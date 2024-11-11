35aa030866686637cf9d5b9813d445f25d904c03
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-12 03:59:30 +0530

    updated correctly

--- server.js
+++ server.js
@@ -1,67 +1,54 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Person = require('./models/person');
-const MenuItem = require('./models/menu')
-
+const MenuItem = require('./models/menu');
+const connectDB = require('./config/db');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/NewHotels')
-.then(() => {
-    console.log("Connected to MongoDB");
-}).catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-});
+connectDB().catch(err => console.error('Database connection error:', err));
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // POST endpoint to add a new menu
 app.post('/menu', async (req, res) => {
   try {
-      const data = req.body;
-      const newMenu = new MenuItem(data);
-      const response = await newMenu.save();
-      res.status(201).json(response);
-      console.log("Response Data Saved");
+    const data = req.body;
+    const newMenu = new MenuItem(data);
+    const response = await newMenu.save();
+    res.status(201).json(response);
+    console.log("Response Data Saved");
   } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ message: "Error Saving Data" });
+    console.error("Error:", error);
+    res.status(500).json({ message: "Error Saving Data" });
   }
 });
 
-//GET for menu
-app.get('/menu', async(req, res)=>{
-try {
-  const data = await MenuItem.find();
-  console.log("Getting the response");
-  res.status(200).json(data);
-} catch (error) {
-  console.log(error);
-  res.status(500).json({message:"Internal server error"});
-}
-
+// GET endpoint for menu
+app.get('/menu', async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    console.log("Getting the response");
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-
-
-// GET
+// Default GET endpoint
 app.get('/', (req, res) => {
-    res.send("Hello, Dwaipayan");
+  res.send("Hello, Dwaipayan");
 });
 
+// Importing routes and using them
 const personRoutes = require('./routes/person.router');
-app.use('/person',personRoutes);
-
+app.use('/person', personRoutes);
 
 app.listen(3000, () => {
   console.log("Server Running on port 3000");
 });
-
-
 
 // app.get('/paneer', (req, res) => {
 //     res.send("Paneer is very tasty");
@@ -76,5 +63,3 @@ app.listen(3000, () => {
 //     };
 //     res.json(customized);
 // });
-
-// Start the server
