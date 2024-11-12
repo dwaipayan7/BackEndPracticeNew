aafd468de17b828e48fdb24b2ac3766cd45d12cc
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-13 03:04:04 +0530

    data updated successfully

--- routes/person.router.js
+++ routes/person.router.js
@@ -44,5 +44,34 @@ router.get("/", async (req, res) => {
   }
 });
 
+//PUT for update
+
+router.put("/:id", async (req, res) => {
+
+  try {
+    
+    const personId = req.params.id;
+    const updatePersonData = req.body;
+
+    const response = await Person.findByIdAndUpdate(personId,updatePersonData, {
+      new: true,
+      runValidators: true
+    })
+
+    if(!response){
+      return res.status(404).json({message: "Person not found"})
+    }
+
+    console.log("Data Updated")
+    res.status(200).json(response)
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+
+})
+
+
 
 module.exports = router;
