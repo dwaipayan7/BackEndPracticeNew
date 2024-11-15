98a7f775b707ff11d202e64117f2ae4007d12f1b
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-15 08:42:02 +0530

    updated

---
+++ auth/auth.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// Passport strategy
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        console.log('Received credentials: ', username, password);
+        
+        // Await the result of the database query
+        const user = await Person.findOne({ username: username });
+        
+        if (!user) {
+          return done(null, false, { message: "Incorrect Username" });
+        }
+  
+        const isPasswordMatch = user.password === password;
+  
+        if (isPasswordMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect Password" });
+        }
+        
+      } catch (error) {
+        return done(error);
+      }
+    })
+  );
+
+  module.exports = passport
--- server.js
+++ server.js
@@ -1,54 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MenuItem = require('./models/menu');
-const Person = require('./models/person')
 const connectDB = require('./config/db');
 const app = express();
-const passport = require('passport')
-const LocalStrategy = require('passport').Strategy
+const passport = require('./auth/auth')
 
 // Connect to MongoDB
 connectDB().catch(err => console.error('Database connection error:', err));
 
-
-//Middleware Function
-
-const logRequest = (req, res, next) =>{
-  console.log(`[${new Date().toLocaleString()}] Request Made to: "${req.originalUrl}"`)
-  next()
-}
-
+// Middleware Function
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request Made to: "${req.originalUrl}"`);
+  next();
+};
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(logRequest);
 
-//passport
-app.use(new LocalStrategy(async (username, password, done)=>{
+// Initialize Passport
+app.use(passport.initialize());
 
-  try {
 
-    console.log('Received credentials: ', username, password);
-    const user = Person.findOne({username:username});
-    if(!user){
-      return done(null,false, {message:"Incorrect User"})
-    }
-
-    const isPasswordMatch = user.password === password ? true:false;
-
-    if (isPasswordMatch) {
-      return done(null,user)
-    }else{
-      return done(null,false, {message:"Incorrect User"})
-    }
-    
-  } catch (error) {
-    return done(error)
-  }
-
-}));
+const localAuthMiddleware = passport.authenticate('local', {session:false})
 
 // Default GET endpoint
 app.get('/', (req, res) => {
@@ -60,22 +35,8 @@ const personRoutes = require('./routes/person.router');
 app.use('/person', personRoutes);
 
 const menuRoutes = require('./routes/menu.router');
-app.use('/menu', menuRoutes)
+app.use('/menu',localAuthMiddleware ,menuRoutes);
 
 app.listen(3000, () => {
   console.log("Server Running on port 3000");
 });
-
-// app.get('/paneer', (req, res) => {
-//     res.send("Paneer is very tasty");
-// });
-
-// app.get('/idli', (req, res) => {
-//     const customized = {
-//         name: 'Paneer Dosa',
-//         size: '10 cm',
-//         is_sambar: true,
-//         is_chutney: false
-//     };
-//     res.json(customized);
-// });
